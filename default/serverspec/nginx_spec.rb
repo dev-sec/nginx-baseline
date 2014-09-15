95e3abfa1fb37d1dbe7e61e467fbd665c480ca9a
Dominik Richter <dominik.richter@gmail.com>
2014-09-15 14:19:08 +0200

    update nginx with tests and requirements
    Signed-off-by: Dominik Richter <dominik.richter@gmail.com>

--- default/serverspec/nginx_spec.rb
+++ default/serverspec/nginx_spec.rb
@@ -36,6 +36,21 @@ nginx_hardening = File.join(nginx_confd, '90.hardening.conf')
 conf_paths      = [ nginx_conf, nginx_hardening ]
 
 # check for files
+describe 'nginx core configuration' do
+
+  describe file(nginx_conf) do
+    it { should be_owned_by 'root' }
+    it { should be_grouped_into 'root' }
+    it { should_not be_readable.by('others') }
+    it { should_not be_writable.by('others') }
+    it { should_not be_executable.by('others') }
+  end
+
+  # ... find stuff in conf.d and sites-available and sites-enabled + both folders
+  # suid / sgid bits
+
+end
+
 describe 'nginx default files' do
   describe file(File.join(nginx_confd, 'default.conf')) do
     it { should_not be_file }
@@ -55,54 +70,77 @@ end
 # check configuration parameters
 describe 'check nginx configuration' do
 
+  
+  describe file(nginx_conf) do
+    its(:content) { should_not match(/^\s*user root;$/) }
+  end
+
+  
+  describe file(nginx_conf) do
+    its(:content) { should_not match(/^\s*group root;$/) }
+  end
+
+  
   describe nginx_conf(conf_paths) do
     its(:content) { should match(/^\s*server_tokens off;$/) }
   end
 
+  
   describe nginx_conf(conf_paths) do
     its(:content) { should match(/^\s*client_body_buffer_size 1k;$/) }
   end
 
+  
   describe nginx_conf(conf_paths) do
     its(:content) { should match(/^\s*client_max_body_size 1k;$/) }
   end
 
+  
   describe nginx_conf(conf_paths) do
     its(:content) { should match(/^\s*keepalive_timeout\s+5 5;$/) }
   end
 
+  
   describe nginx_conf(conf_paths) do
     its(:content) { should match(/^\s*more_clear_headers 'Server';$/) }
   end
 
+  
   describe nginx_conf(conf_paths) do
     its(:content) { should match(/^\s*more_clear_headers 'X-Powered-By';$/) }
   end
 
+  
   describe nginx_conf(conf_paths) do
     its(:content) { should match(/^\s*client_header_buffer_size 1k;$/) }
   end
 
+  
   describe nginx_conf(conf_paths) do
     its(:content) { should match(/^\s*large_client_header_buffers 2 1k;$/) }
   end
 
+  
   describe nginx_conf(conf_paths) do
     its(:content) { should match(/^\s*client_body_timeout 10;$/) }
   end
 
+  
   describe nginx_conf(conf_paths) do
     its(:content) { should match(/^\s*client_header_timeout 10;$/) }
   end
 
+  
   describe nginx_conf(conf_paths) do
     its(:content) { should match(/^\s*send_timeout 10;$/) }
   end
 
+  
   describe nginx_conf(conf_paths) do
     its(:content) { should match(/^\s*limit_conn_zone \$binary_remote_addr zone=default:10m;$/) }
   end
 
+  
   describe nginx_conf(conf_paths) do
     its(:content) { should match(/^\s*limit_conn default 5;$/) }
   end
