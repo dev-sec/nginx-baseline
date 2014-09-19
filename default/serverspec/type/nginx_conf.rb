2acd8a2bec97ba24c2d56ffb0effcceefa2369d1
Patrick Meier <patrick.meier111@googlemail.com>
2014-09-19 15:06:59 +0200

    fix rubocop issues
    Signed-off-by: Patrick Meier <patrick.meier111@googlemail.com>

--- default/serverspec/type/nginx_conf.rb
+++ default/serverspec/type/nginx_conf.rb
@@ -1,22 +1,17 @@
 # -*- coding: utf-8 -*-
 module Serverspec
   module Type
-
     class NginxConf < Base
-  
       def initialize(paths)
         @paths = paths
       end
-     
       def content
-        @paths.map{ |x| ::File.read x }.join("\n")
+        @paths.map { |x| ::File.read x }.join("\n")
       end
     end
-
     def nginx_conf(paths)
       NginxConf.new(paths)
     end
   end
 end
-
 include Serverspec::Type
