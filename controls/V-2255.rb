a1488e147f5b5c18cf39517b108cb0e85a9dc28c
Rony Xavier <rx294@nyu.edu>
2017-06-08 17:41:00 -0400

    V-2255.rb update

--- controls/V-2255.rb
+++ controls/V-2255.rb
@@ -1,25 +1,66 @@
-title 'V-2255 - The web server’s htpasswd files (if present) must reflect proper ownership and permissions'
-control 'V-2255' do
+# encoding: utf-8 
+# 
+=begin 
+----------------- 
+Benchmark: APACHE SERVER 2.2 for Unix  
+Status: Accepted 
+
+All directives specified in this STIG must be specifically set (i.e. the
+server is not allowed to revert to programmed defaults for these directives).
+Included files should be reviewed if they are used. Procedures for reviewing
+included files are included in the overview document. The use of .htaccess
+files are not authorized for use according to the STIG. However, if they are
+used, there are procedures for reviewing them in the overview document. The
+Web Policy STIG should be used in addition to the Apache Site and Server STIGs
+in order to do a comprehensive web server review.
+
+Release Date: 2015-08-28 
+Version: 1 
+Publisher: DISA 
+Source: STIG.DOD.MIL 
+uri: http://iase.disa.mil 
+----------------- 
+=end 
+
+
+only_if do
+  command('nginx').exist?
+end
+
+
+control "V-2255" do
+  title "The web server’s htpasswd files (if present) must reflect proper ownership and permissions"
+  
+  desc "In addition to OS restrictions, access rights to files and directories
+  can be set on a web site using the web server software.That is, in addition
+  to allowing or denying all access rights, a rule can be specified that
+  allows or denies partial access rights.For example, users can be given read-
+  only access rights to files, to view the information but not change the
+  files.
+
+  This check verifies that the htpasswd file is only accessible by system
+  administrators or web managers, with the account running the web service
+  having group permissions of read and execute.  htpasswd is a utility used to
+  provide for password access to web sites."
+  
   impact 0.5
-  title 'The web server’s htpasswd files (if present) must reflect proper ownership and permissions'
-  desc '"In addition to OS restrictions, access rights to files and directories can be set on a web site using the web server software.  That is, in addition to allowing or denying all access rights, a rule can be specified that allows or denies partial access rights.  For example, users can be given read-only access rights to files, to view the information but not change the files. This check verifies that the htpasswd file is only accessible by system administrators or web managers, with the account running the web service having group permissions of read and execute.  htpasswd is a utility used to provide for password access to web sites."'
-  tag 'stig', 'V-2255'
-  tag severity: 'medium'
-  tag checkid: 'C-2672r2_chk'
-  tag fixid: 'F-6758r2_fix'
-  tag version: 'WG270 A22'
-  tag ruleid: 'SV-36478r2_rule'
-  tag nist: 'AC-3'
-  tag fixtext: 'The SA or Web Manager account should own the htpasswd file and permissions should be set to 550.'
-  tag checktext: 'To locate the htpasswd file enter the following command:
+  tag "severity": "medium"
+  tag "gtitle": "WG270"
+  tag "gid": "V-2255"
+  tag "rid": "SV-36478r2_rule"
+  tag "stig_id": "WG270 A22"
+  tag "nist": ["AC-3", "Rev_4"]
+  tag "check": "To locate the htpasswd file enter the following command:
 
-Find / -name htpasswd
-Permissions should be r-x r - x - - - (550)
+  Find / -name htpasswd Permissions should be r-x r - x - - - (550)
 
-If permissions on htpasswd are greater than 550, this is a finding.
+  If permissions on htpasswd are greater than 550, this is a finding.
 
-Owner should be the SA or Web Manager account, if another account has access to this file, this is a finding.
-'
+  Owner should be the SA or Web Manager account, if another account has access
+  to this file, this is a finding. " 
+
+  tag "fix": "The SA or Web Manager account
+  should own the htpasswd file and permissions should be set to 550." 
 
 # START_DESCRIBE V-2255
   htpasswd = command('find / -name htpasswd').stdout.chomp
@@ -31,4 +72,3 @@ Owner should be the SA or Web Manager account, if another account has access to 
 # STOP_DESCRIBE V-2255
 
 end
-
