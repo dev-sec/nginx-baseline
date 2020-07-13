86d60e68aadac793220518ece870a417a0a7970a
Enzo Mignogna <enzo.mignogna@avature.net>
2020-07-13 16:07:16 +0100

    Change default: to value:
    Signed-off-by: Enzo Mignogna <enzo.mignogna@avature.net>

--- controls/nginx_spec.rb
+++ controls/nginx_spec.rb
@@ -24,67 +24,67 @@ title 'NGINX server config'
 CLIENT_MAX_BODY_SIZE = attribute(
   'client_max_body_size',
   description: 'Sets the maximum allowed size of the client request body, specified in the “Content-Length” request header field. If the size in a request exceeds the configured value, the 413 (Request Entity Too Large) error is returned to the client. Please be aware that browsers cannot correctly display this error. Setting size to 0 disables checking of client request body size.',
-  default: '1k'
+  value: '1k'
 )
 
 CLIENT_BODY_BUFFER_SIZE = attribute(
   'client_body_buffer_size',
   description: 'Sets buffer size for reading client request body. In case the request body is larger than the buffer, the whole body or only its part is written to a temporary file. By default, buffer size is equal to two memory pages. This is 8K on x86, other 32-bit platforms, and x86-64. It is usually 16K on other 64-bit platforms.',
-  default: '1k'
+  value: '1k'
 )
 
 CLIENT_HEADER_BUFFER_SIZE = attribute(
   'client_header_buffer_size',
   description: 'Sets buffer size for reading client request header. For most requests, a buffer of 1K bytes is enough. However, if a request includes long cookies, or comes from a WAP client, it may not fit into 1K. If a request line or a request header field does not fit into this buffer then larger buffers, configured by the large_client_header_buffers directive, are allocated.',
-  default: '1k'
+  value: '1k'
 )
 
 LARGE_CLIENT_HEADER_BUFFER = attribute(
   'large_client_header_buffers',
   description: 'Sets the maximum number and size of buffers used for reading large client request header. A request line cannot exceed the size of one buffer, or the 414 (Request-URI Too Large) error is returned to the client. A request header field cannot exceed the size of one buffer as well, or the 400 (Bad Request) error is returned to the client. Buffers are allocated only on demand. By default, the buffer size is equal to 8K bytes. If after the end of request processing a connection is transitioned into the keep-alive state, these buffers are released.',
-  default: '2 1k'
+  value: '2 1k'
 )
 
 KEEPALIVE_TIMEOUT = attribute(
   'keepalive_timeout',
   description: 'The first parameter sets a timeout during which a keep-alive client connection will stay open on the server side. The zero value disables keep-alive client connections. The optional second parameter sets a value in the “Keep-Alive: timeout=time” response header field. Two parameters may differ.',
-  default: '5 5'
+  value: '5 5'
 )
 
 CLIENT_BODY_TIMEOUT = attribute(
   'client_body_timeout',
   description: 'Defines a timeout for reading client request body. The timeout is set only for a period between two successive read operations, not for the transmission of the whole request body. If a client does not transmit anything within this time, the 408 (Request Time-out) error is returned to the client.',
-  default: '10'
+  value: '10'
 )
 
 CLIENT_HEADER_TIMEOUT = attribute(
   'client_header_timeout',
   description: 'Defines a timeout for reading client request header. If a client does not transmit the entire header within this time, the 408 (Request Time-out) error is returned to the client.',
-  default: '10'
+  value: '10'
 )
 
 SEND_TIMEOUT = attribute(
   'send_timeout',
   description: 'Sets a timeout for transmitting a response to the client. The timeout is set only between two successive write operations, not for the transmission of the whole response. If the client does not receive anything within this time, the connection is closed.',
-  default: '10'
+  value: '10'
 )
 
 HTTP_METHODS = attribute(
   'http_methods',
   description: 'Specify the used HTTP methods',
-  default: 'GET\|HEAD\|POST'
+  value: 'GET\|HEAD\|POST'
 )
 
 HTTP_METHODS_CHECK = attribute(
   'http_methods_check',
   description: 'Defines if http_methods should be checked in the nginx configuration',
-  default: false
+  value: false
 )
 
 NGINX_COOKIE_FLAG_MODULE = attribute(
   'nginx_cookie_flag_module',
   description: 'Defines if nginx has been compiled with nginx_cookie_flag_module',
-  default: false
+  value: false
 )
 
 only_if do
@@ -190,7 +190,7 @@ control 'nginx-07' do
   title 'Control simultaneous connections'
   desc 'NginxHttpLimitZone module to limit the number of simultaneous connections for the assigned session or as a special case, from one IP address.'
   describe parse_config(nginx_parsed_config, options) do
-    its('limit_conn_zone') { should eq '$binary_remote_addr zone=default:10m' }
+    its('limit_conn_zone') { should eq '$binary_remote_addr zone=value:10m' }
   end
   describe parse_config(nginx_parsed_config, options) do
     its('limit_conn') { should eq 'default 5' }
